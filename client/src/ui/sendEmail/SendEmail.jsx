b881caeb7c7f9d3e37d4ddce871ce68f06b02785
romanchykov-webdev <romanchykov.webdev@gmail.com>
2023-12-29 19:42:10 +0100

    input email
    refactoring component sendEmail

--- client/src/ui/sendEmail/SendEmail.jsx
+++ client/src/ui/sendEmail/SendEmail.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './SendEmail.module.scss'
 
 
 const SendEmail = () => {
+
+    const [inputEmail, setInputEmail] = useState('')
+
     return (
         <div className={s.blockSendEmail}>
-            <input type="text" placeholder="Enter email..."/>
-            <button className={s.btnSendMail}>
-                Subscribe
-            </button>
+            <form >
+                <input
+                    value={inputEmail}
+                    onChange={(e) => setInputEmail(e.target.value)}
+                    type="text"
+                    placeholder="Enter email..."
+                />
+                <button
+                    type={'submit'}
+                    className={s.btnSendMail}
+                >
+                    Subscribe
+                </button>
+            </form>
         </div>
     );
 };
